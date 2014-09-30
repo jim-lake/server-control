1c1a6e8f8d4869340c7308288ea08439483efc9d
Jim Lake <jim@blueskylabs.com>
2014-09-30 13:07:53 -0700

    rename api_update

--- instance_update.sh
+++ instance_update.sh
@@ -3,8 +3,8 @@ source <( curl "http://169.254.169.254/latest/user-data" 2>/dev/null )
  
 if [ "${NODE_GIT_HASH}" != "" ]; then
   pushd $PROJECT_DIR >/dev/null 2>/dev/null
-  su node -c "./git_update_to_hash.sh ${NODE_GIT_HASH}" >>/tmp/api-update.log 2>&1
-  su node -c "npm install" >>/tmp/npm-update.log 2>&1
+  su node -c "./git_update_to_hash.sh ${NODE_GIT_HASH}" >>/tmp/instance-update.log 2>&1
+  su node -c "npm install" >>/tmp/instance-update-npm.log 2>&1
   popd >/dev/null 2>/dev/null
   /usr/bin/supervisorctl restart all
 fi
